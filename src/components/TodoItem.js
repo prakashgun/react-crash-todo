a6f17b07a3650e0e8e735969d553983225988a8e
prakashgun <mail.prakash.india@gmail.com>
2019-06-02 22:49:20 +0530

    Refactored code

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
 
     getStyle = () => {
-        if (this.props.todo.completed) {
-            return {
-                textDecoration: 'line-through'
-            }
-        } else {
-            return {
-                textDecoration: 'none'
-            }
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
         }
-    }
+    };
 
     render() {
         return (
@@ -26,7 +23,7 @@ export class TodoItem extends Component {
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
-}
+};
 
 export default TodoItem
 
